99e52938d8dbdec9fe5b5e654f21389d1d87230d
gottsch <17928819+gottsch@users.noreply.github.com>
2018-03-07 21:10:36 -0500

    Added different success probabilities for the different RARITYs.

--- Treasure2-1.12.2/src/com/someguyssoftware/treasure2/item/PilferersLockPick.java
+++ Treasure2-1.12.2/src/com/someguyssoftware/treasure2/item/PilferersLockPick.java
@@ -4,8 +4,11 @@
 package com.someguyssoftware.treasure2.item;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
+import com.someguyssoftware.gottschcore.random.RandomHelper;
+import com.someguyssoftware.treasure2.Treasure;
 import com.someguyssoftware.treasure2.enums.Category;
 import com.someguyssoftware.treasure2.enums.Rarity;
 
@@ -57,5 +60,27 @@ public class PilferersLockPick extends KeyItem {
 		return false;
 	}	
 
-	// TODO implement unlock using different success probabilities for COMMON and UNCOMMON
+	/*
+	 * If UNCOMMON lock, then this key has 50% less chance (ie x/2) of succeeding
+	 * @see com.someguyssoftware.treasure2.item.KeyItem#unlock(com.someguyssoftware.treasure2.item.LockItem)
+	 */
+	@Override
+	public boolean unlock(LockItem lockItem) {
+		if (lockItem.acceptsKey(this) || fitsLock(lockItem)) {
+			Treasure.logger.debug("Lock accepts key");
+			if (lockItem.getRarity() == Rarity.UNCOMMON) {
+				if (RandomHelper.checkProbability(new Random(), this.getSuccessProbability()/2)) {
+					Treasure.logger.debug("Unlock attempt met probability");
+					return true;
+				}				
+			}
+			else {
+				if (RandomHelper.checkProbability(new Random(), this.getSuccessProbability())) {
+					Treasure.logger.debug("Unlock attempt met probability");
+					return true;
+				}
+			}			
+		}
+		return false;
+	}
 }
