67c1351f51c02fddd605ac779d5d369c857f5a65
gottsch <17928819+gottsch@users.noreply.github.com>
2019-07-09 06:46:12 -0400

    Register Foreign mod loot tables
    Fix foreign mod loot tables internal references to other loot tables.

--- Treasure2-1.12.2/src/com/someguyssoftware/treasure2/eventhandler/WorldEventHandler.java
+++ Treasure2-1.12.2/src/com/someguyssoftware/treasure2/eventhandler/WorldEventHandler.java
@@ -6,9 +6,11 @@ package com.someguyssoftware.treasure2.eventhandler;
 import com.someguyssoftware.gottschcore.mod.IMod;
 import com.someguyssoftware.gottschcore.world.WorldInfo;
 import com.someguyssoftware.treasure2.Treasure;
+import com.someguyssoftware.treasure2.config.TreasureConfig;
 
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
@@ -40,6 +42,12 @@ public class WorldEventHandler {
 //			TreasureLootTables.init(world);
 			Treasure.LOOT_TABLES.init(world);
 			Treasure.LOOT_TABLES.register(getMod().getId());
+			// register any foreign mod loot tables
+			for (String foreignModID : TreasureConfig.enableForeignModIDs) {
+				if (Loader.isModLoaded(foreignModID)) {		
+					Treasure.LOOT_TABLES.register(foreignModID);
+				}
+			}
 		}
 	}
 
