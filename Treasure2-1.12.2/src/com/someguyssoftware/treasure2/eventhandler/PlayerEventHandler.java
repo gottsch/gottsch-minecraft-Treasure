427ee78c210345360886081d0aef7ab5f606278a
gottsch <17928819+gottsch@users.noreply.github.com>
2020-02-08 16:04:26 -0500

    -initial commit of patchouli integration.

--- Treasure2-1.12.2/src/com/someguyssoftware/treasure2/eventhandler/PlayerEventHandler.java
+++ Treasure2-1.12.2/src/com/someguyssoftware/treasure2/eventhandler/PlayerEventHandler.java
@@ -9,6 +9,7 @@ import com.someguyssoftware.treasure2.Treasure;
 import com.someguyssoftware.treasure2.block.FogBlock;
 import com.someguyssoftware.treasure2.block.WitherFogBlock;
 import com.someguyssoftware.treasure2.client.gui.inventory.KeyRingGui;
+import com.someguyssoftware.treasure2.config.TreasureConfig;
 import com.someguyssoftware.treasure2.enums.FogType;
 import com.someguyssoftware.treasure2.inventory.KeyRingContainer;
 import com.someguyssoftware.treasure2.inventory.KeyRingInventory;
@@ -21,20 +22,30 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 /**
  * @author Mark Gottschling on Apr 26, 2018
  *
  */
-public class PlayerEventHandler {
+public class PlayerEventHandler {	
+	private static final String FIRST_JOIN_NBT_KEY = "treasure2.firstjoin";
+	private static final String PATCHOULI_MODID = "patchouli";
+	private static final String PATCHOULI_GUIDE_BOOK_ID = "patchouli:guide_book";
+	private static final String PATCHOULI_GUIDE_TAG_ID = "patchouli:book";
+	private static final String TREASURE2_GUIDE_TAG_VALUE = "treasure2:guide";
+	
 	// reference to the mod.
 	private IMod mod;
 	
@@ -45,6 +56,43 @@ public class PlayerEventHandler {
 		setMod(mod);
 	}
 	
+	
+    @SubscribeEvent
+    public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+    	// check if config is enabled
+        if (!TreasureConfig.MOD.enableStartingBook) {
+            return;
+        }
+
+        if (event.player.isCreative()) {
+        	return;
+        }
+        
+        NBTTagCompound data = event.player.getEntityData();
+        NBTTagCompound persistent;
+        if (!data.hasKey(EntityPlayer.PERSISTED_NBT_TAG)) {
+        	data.setTag(EntityPlayer.PERSISTED_NBT_TAG, (persistent = new NBTTagCompound()));
+        } else {
+            persistent = data.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+        }
+        
+        // check if the patchouli mod is installed
+        if (Loader.isModLoaded(PATCHOULI_MODID)) {
+	        if (!persistent.hasKey(FIRST_JOIN_NBT_KEY)) {
+	            persistent.setBoolean(FIRST_JOIN_NBT_KEY, true);
+	            // create the book
+	            Item guideBook = Item.REGISTRY.getObject(new ResourceLocation(PATCHOULI_GUIDE_BOOK_ID));
+	           ItemStack stack = new ItemStack(guideBook);
+	           if (!stack.hasTagCompound()) {
+	        	   NBTTagCompound tag = new NBTTagCompound();
+	        	   tag.setString(PATCHOULI_GUIDE_TAG_ID, TREASURE2_GUIDE_TAG_VALUE);
+	        	   stack.setTagCompound(tag);
+	           }
+	            event.player.inventory.addItemStackToInventory(stack);
+	        }
+        }
+    }
+    
 	/**
 	 * 
 	 * @param event
