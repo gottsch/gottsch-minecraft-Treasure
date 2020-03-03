93514fa8b0db8b3cd0926d08d79e8516cbb77021
gottsch <17928819+gottsch@users.noreply.github.com>
2020-03-03 07:09:02 -0500

    temporarily disabling patchouli books

--- Treasure2-1.12.2/src/com/someguyssoftware/treasure2/eventhandler/PlayerEventHandler.java
+++ Treasure2-1.12.2/src/com/someguyssoftware/treasure2/eventhandler/PlayerEventHandler.java
@@ -5,31 +5,20 @@ package com.someguyssoftware.treasure2.eventhandler;
 
 import com.someguyssoftware.gottschcore.mod.IMod;
 import com.someguyssoftware.gottschcore.world.WorldInfo;
-import com.someguyssoftware.treasure2.Treasure;
 import com.someguyssoftware.treasure2.block.FogBlock;
-import com.someguyssoftware.treasure2.block.WitherFogBlock;
-import com.someguyssoftware.treasure2.client.gui.inventory.KeyRingGui;
 import com.someguyssoftware.treasure2.config.TreasureConfig;
 import com.someguyssoftware.treasure2.enums.FogType;
-import com.someguyssoftware.treasure2.inventory.KeyRingContainer;
-import com.someguyssoftware.treasure2.inventory.KeyRingInventory;
-import com.someguyssoftware.treasure2.item.KeyRingItem;
-import com.someguyssoftware.treasure2.item.TreasureItems;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -39,70 +28,70 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
  * @author Mark Gottschling on Apr 26, 2018
  *
  */
-public class PlayerEventHandler {	
+public class PlayerEventHandler {
 	private static final String FIRST_JOIN_NBT_KEY = "treasure2.firstjoin";
 	private static final String PATCHOULI_MODID = "patchouli";
 	private static final String PATCHOULI_GUIDE_BOOK_ID = "patchouli:guide_book";
 	private static final String PATCHOULI_GUIDE_TAG_ID = "patchouli:book";
 	private static final String TREASURE2_GUIDE_TAG_VALUE = "treasure2:guide";
-	
+
 	// reference to the mod.
 	private IMod mod;
-	
+
 	/**
 	 * 
 	 */
 	public PlayerEventHandler(IMod mod) {
 		setMod(mod);
 	}
-	
-	
-    @SubscribeEvent
-    public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
-    	// check if config is enabled
-        if (!TreasureConfig.MOD.enableStartingBook) {
-            return;
-        }
 
-        if (event.player.isCreative()) {
-        	return;
-        }
-        
-        NBTTagCompound data = event.player.getEntityData();
-        NBTTagCompound persistent;
-        if (!data.hasKey(EntityPlayer.PERSISTED_NBT_TAG)) {
-        	data.setTag(EntityPlayer.PERSISTED_NBT_TAG, (persistent = new NBTTagCompound()));
-        } else {
-            persistent = data.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
-        }
-        
-        // check if the patchouli mod is installed
-        if (Loader.isModLoaded(PATCHOULI_MODID)) {
-	        if (!persistent.hasKey(FIRST_JOIN_NBT_KEY)) {
-	            persistent.setBoolean(FIRST_JOIN_NBT_KEY, true);
-	            // create the book
-	            Item guideBook = Item.REGISTRY.getObject(new ResourceLocation(PATCHOULI_GUIDE_BOOK_ID));
-	           ItemStack stack = new ItemStack(guideBook);
-	           if (!stack.hasTagCompound()) {
-	        	   NBTTagCompound tag = new NBTTagCompound();
-	        	   tag.setString(PATCHOULI_GUIDE_TAG_ID, TREASURE2_GUIDE_TAG_VALUE);
-	        	   stack.setTagCompound(tag);
-	           }
-	            event.player.inventory.addItemStackToInventory(stack);
-	        }
-        }
-    }
-    
+//	TEMP remove until patchouli book is complete.
+//    @SubscribeEvent
+	public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+		// check if config is enabled
+		if (!TreasureConfig.MOD.enableStartingBook) {
+			return;
+		}
+
+		if (event.player.isCreative()) {
+			return;
+		}
+
+		NBTTagCompound data = event.player.getEntityData();
+		NBTTagCompound persistent;
+		if (!data.hasKey(EntityPlayer.PERSISTED_NBT_TAG)) {
+			data.setTag(EntityPlayer.PERSISTED_NBT_TAG, (persistent = new NBTTagCompound()));
+		} else {
+			persistent = data.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+		}
+
+		// check if the patchouli mod is installed
+		if (Loader.isModLoaded(PATCHOULI_MODID)) {
+			if (!persistent.hasKey(FIRST_JOIN_NBT_KEY)) {
+				persistent.setBoolean(FIRST_JOIN_NBT_KEY, true);
+				// create the book
+				Item guideBook = Item.REGISTRY.getObject(new ResourceLocation(PATCHOULI_GUIDE_BOOK_ID));
+				ItemStack stack = new ItemStack(guideBook);
+				if (!stack.hasTagCompound()) {
+					NBTTagCompound tag = new NBTTagCompound();
+					tag.setString(PATCHOULI_GUIDE_TAG_ID, TREASURE2_GUIDE_TAG_VALUE);
+					stack.setTagCompound(tag);
+				}
+				event.player.inventory.addItemStackToInventory(stack);
+			}
+		}
+	}
+
 	/**
 	 * 
 	 * @param event
 	 */
 	@SubscribeEvent
 	public void checkFogInteraction(LivingUpdateEvent event) {
-        if (WorldInfo.isClientSide(event.getEntity().getEntityWorld())) {
-        	return;
-        }
-        
+		if (WorldInfo.isClientSide(event.getEntity().getEntityWorld())) {
+			return;
+		}
+
 		// do something to player every update tick:
 		if (event.getEntity() instanceof EntityPlayer) {
 			// get the player
@@ -113,27 +102,30 @@ public class PlayerEventHandler {
 			Block block = event.getEntity().getEntityWorld().getBlockState(pos).getBlock();
 			if (block instanceof FogBlock) {
 				// check the fog type
-				if (((FogBlock)block).getFogType() == FogType.WITHER	) {
-		        	PotionEffect potionEffect = ((EntityLivingBase)event.getEntity()).getActivePotionEffect(MobEffects.WITHER);
-		        	// if player does not have wither effect, add it
-		        	if (potionEffect == null) {
-		        		((EntityLivingBase)event.getEntity()).addPotionEffect(new PotionEffect(MobEffects.WITHER, 300, 0));
+				if (((FogBlock) block).getFogType() == FogType.WITHER) {
+					PotionEffect potionEffect = ((EntityLivingBase) event.getEntity())
+							.getActivePotionEffect(MobEffects.WITHER);
+					// if player does not have wither effect, add it
+					if (potionEffect == null) {
+						((EntityLivingBase) event.getEntity())
+								.addPotionEffect(new PotionEffect(MobEffects.WITHER, 300, 0));
 //		        		Treasure.logger.debug("wither potion effect is null, should be adding....");
-		        	}					
-				}
-				else if (((FogBlock)block).getFogType() == FogType.POISON) {
-		        	PotionEffect potionEffect = ((EntityLivingBase)event.getEntity()).getActivePotionEffect(MobEffects.POISON);
-		        	// if player does not have wither effect, add it
-		        	if (potionEffect == null) {
-		        		((EntityLivingBase)event.getEntity()).addPotionEffect(new PotionEffect(MobEffects.POISON, 300, 0));
+					}
+				} else if (((FogBlock) block).getFogType() == FogType.POISON) {
+					PotionEffect potionEffect = ((EntityLivingBase) event.getEntity())
+							.getActivePotionEffect(MobEffects.POISON);
+					// if player does not have wither effect, add it
+					if (potionEffect == null) {
+						((EntityLivingBase) event.getEntity())
+								.addPotionEffect(new PotionEffect(MobEffects.POISON, 300, 0));
 //		        		Treasure.logger.debug("poison potion effect is null, should be adding....");
-		        	}				
+					}
 				}
 			}
-      	
+
 		}
 	}
-	
+
 	/**
 	 * @return the mod
 	 */
