15c91a993b5b0a64ab391684213eefc4b3b25c23
gottsch <17928819+gottsch@users.noreply.github.com>
2020-12-17 07:24:42 -0500

    Update CharmType.java

--- src/main/java/com/someguyssoftware/treasure2/item/charm/CharmType.java
+++ src/main/java/com/someguyssoftware/treasure2/item/charm/CharmType.java
@@ -12,8 +12,12 @@ public enum CharmType {
 	SHIELDING("shielding", new double[] {20, 50, 100, 200}, new double[] {0.5, 0.6, 0.7, 0.8}),
 	FULLNESS("fullness", new double[] {20, 50, 100, 200}),
 	HARVESTING("harvesting", new double[] {20, 30, 40, 50}, new double[] {2, 3, 4, 5}),	// values here indicate number of harvestable blocks
-	ILLUMINATION("illumination", new double[] {3, 6, 12, 20}),
-	DECAY("decay", new double[] {20, 50, 100, 200});
+	ILLUMINATION("illumination", new double[] {3, 6, 12, 20}),	// values here indicate number of placeable torches 
+	DECAY("decay", new double[] {20, 50, 100, 200}),
+	RUIN("ruin", new double[] {60, 45, 30, 15}), 	// values represent seconds. ever x seconds, a piece of armor/tool is damaged
+	FIRE_RESISTENCE("fire_resist", new double[] {10, 20, 40, 80}),
+	LIFE_STRIKE("life_strike", mew double[] {1, 1, 1, 1}, new double {1.25, 1.5, 1.75, 2});	// vaulves represent the amount of hearts consumed to delivery amount of damage (modifier) 
+	
 	
 	
 	private String name;
