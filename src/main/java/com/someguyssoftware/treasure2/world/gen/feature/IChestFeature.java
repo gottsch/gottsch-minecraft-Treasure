cfaca041f90e3d6a9102917bad6bf6245658b2fe
gottsch <mark.gottschling@gmail.com>
2022-09-21 18:24:33 +0000

    comments

--- src/main/java/com/someguyssoftware/treasure2/world/gen/feature/IChestFeature.java
+++ src/main/java/com/someguyssoftware/treasure2/world/gen/feature/IChestFeature.java
@@ -8,6 +8,7 @@ public interface IChestFeature extends ITreasureFeature {
 	 * @param spawnCoords
 	 * @return
 	 */
+	 // TODO need to pass in the key (surface, submerged, wither, etc)
 	default public boolean meetsProximityCriteria(ServerWorld world, ResourceLocation dimension, ICoords spawnCoords) {
 		if (ITreasureFeature.isRegisteredChestWithinDistance(world, dimension, "surface", spawnCoords, 
 				TreasureConfig.CHESTS.surfaceChestGen.minBlockDistance.get())) {
